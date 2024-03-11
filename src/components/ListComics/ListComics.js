90d3d48af99e18bdd98521d4306fc39e68bef06e
Nicolás Teresa <nicolas.teresa1999.to@gmail.com>
2024-03-11 11:14:11 +0100

    Añadida lista de comics

---
+++ src/components/ListComics/ListComics.js
@@ -0,0 +1,54 @@
+import "./ListComics.scss";
+import { Card, Icon, Image, Dimmer, Loader, Button } from "semantic-ui-react";
+
+export default function ListComics({listComics, renderComics, setRenderComics}) {
+const { loading, result } = listComics;
+
+if (loading || !result) {
+    return (
+        <Dimmer active inverted>
+            <Loader inverted>
+                Loading...
+            </Loader>
+        </Dimmer>
+    );
+}
+
+const { results } = result.data;
+
+const loadMoreComics = () => {
+    const numberComics = renderComics;
+    setRenderComics(numberComics + 5);
+  };
+  
+return (
+    <Card.Group itemsPerRow={5}>
+        {results.map((res, index) => (
+            <Card key={index} className="list-comics">
+                <Image src={`${res.images[0].path}.${res.images[0].extension}`} wrapped ui={false} />
+                <Card.Content>
+                    <Card.Header>{res.title}</Card.Header>
+                    <Card.Meta>
+                        <span>Digital Id: {res.id}</span>
+                    </Card.Meta>
+                </Card.Content>
+                <Card.Content>
+                    <Button animates fluid as="a" href={res.urls[0].url} target="_blank" color="black">
+                        <Button.Content visible>Más información</Button.Content>
+                        <Button.Content hidden>
+                            <Icon name="arrow rigth" />
+                        </Button.Content>
+                    </Button>
+                </Card.Content>
+            </Card>
+        ))}
+         <div className="container-button">
+        <Button color="red" onClick={() => loadMoreComics()}>
+          <Icon name="plus" />
+          Cargar más cómics
+        </Button>
+      </div>
+    </Card.Group>
+);
+
+}
